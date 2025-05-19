6495abbef8d3bc94d55c894ce949f34c38f9101c
thejacknives <afonso.gouveia.melo@gmail.com>
2025-05-19 21:53:40 +0100

    deleting the temp files

--- prime_backend/accounts/views.py
+++ prime_backend/accounts/views.py
@@ -9,21 +9,28 @@ from .utils.aws import upload_image_to_s3, search_face_s3
 from .models import User
 from django.contrib.auth import authenticate
 from accounts.utils.auth import generate_jwt
+from accounts.utils.aws import s3_client, bucket
 
 @api_view(['POST'])
 def login_with_face(request):
     face_image = request.FILES['face_image']
-    temp_key = f'temp_login/{uuid.uuid4()}.jpg'
+    temp_key = f'faces/{uuid.uuid4()}.jpg'
     upload_image_to_s3(face_image, temp_key)
 
     match = search_face_s3(temp_key)
     if not match:
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         return Response({'error': 'No face match found'}, status=404)
 
-    try:
+    try:        
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         user = User.objects.get(face_id=match)
         return Response({'message': f'Welcome back, {user.username}!'})
     except User.DoesNotExist:
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         return Response({'error': 'Face ID not recognized'}, status=403)
 
 @api_view(['POST'])
@@ -54,17 +61,23 @@ def login_with_face(request):
         return Response({'error': 'No face image uploaded'}, status=400)
 
     # Upload to S3 with a temp key
-    temp_key = f'temp_logins/{uuid.uuid4()}.jpg'
+    temp_key = f'faces/{uuid.uuid4()}.jpg'
     upload_image_to_s3(face_image, temp_key)
 
     # Rekognition: search by image
     face_id = search_face_s3(temp_key)
     if not face_id:
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         return Response({'error': 'Face not recognized'}, status=403)
 
     try:
         user = User.objects.get(face_id=face_id)
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         token = generate_jwt(user)
         return Response({'token':token, 'message': f'Welcome back, {user.username}!'}, status=200)
     except User.DoesNotExist:
+        s3_client.delete_object(Bucket=bucket, Key=temp_key)
+
         return Response({'error': 'User not found for face ID'}, status=404)
