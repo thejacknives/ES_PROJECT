a8a685e22e3cc41b969cae35546190c8c0878f1d
GeneralTT <tomas.tavares7@gmail.com>
2025-05-30 20:19:01 +0100

    fix

--- prime_backend/prime_backend/urls.py
+++ prime_backend/prime_backend/urls.py
@@ -31,10 +31,7 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include([
-        path('accounts/', include('accounts.urls')),
-
-    ])),
+    path('api/', include('accounts.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
